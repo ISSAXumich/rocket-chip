deb8f3d501d36b2bff8cf0d790a01bf3b6e6e7e5
Brendan West <westbl@umich.edu>
2017-11-02 12:26:18 -0400

    Fixed scala files (added mod instruction)

---
+++ emulator/mod.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int a[] = {11,22,33,44,55};
+int b[] = {10,10,10,10,10};
+int c[5];
+
+int main() {
+  int i;
+  for( i = 0; i < 5; i++ ) {
+     c[i] = 0;
+  }
+
+  for( i = 0; i < 5; i++ ) {
+     asm volatile
+     (
+       "mod   %[z], %[x], %[y]\n\t"
+       : [z] "=r" (c[i])
+       : [x] "r" (a[i]), [y] "r" (b[i])
+     );
+  }
+ 
+  for ( int i = 0; i < 5; i++ ) {
+      if( c[i] != i+1 ) {
+         printf("\n[[FAILED]]: i=%d, c[i]=%d\n",i,c[i]);
+         return -1;
+      }
+      printf("i=%d, c[i]=%d\n",i,c[i]);
+  }
+  
+  printf("\n[[PASSED]]\n");
+
+  return 0;
+}
